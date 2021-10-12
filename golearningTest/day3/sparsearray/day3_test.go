8cbd45bf04b36febc16d3d563078981f21d86c18
Tony1sun <jacktony822@gmail.com>
2021-10-12 12:38:32 +0800

    提交

---
+++ golearningTest/day3/sparsearray/day3_test.go
@@ -0,0 +1,21 @@
+package sparsearray
+
+import "testing"
+
+// func TesttypeSwitch(t *testing.T) {
+// 	a := typeSwitch(10)
+// 	t.Log(a)
+// }
+
+func TestMap(t *testing.T) {
+	numbers := []base{1, 2, 3, 4}
+	strings := []base{"a", "b", "c"}
+	newNumbers := Map(numbers, typeSwitch)
+	newStrings := Map(strings, typeSwitch)
+	t.Log(newNumbers)
+	t.Log(newStrings)
+}
+
+func TestCat(t *testing.T) {
+	Cat("log.txt", true)
+}
