5d2309dc22abbacb187ac00bf4eb365ad274179b
Tony1sun <jacktony822@gmail.com>
2021-10-19 23:01:27 +0800

    add

--- day3/selectsort/main.go
+++ day3/selectsort/main.go
@@ -7,7 +7,7 @@ import "fmt"
 // 注释
 func SelectSort(arr *[6]int) {
 	// 1.假设 arr[0] 是最大值
-	for j := 0; j < len(arr) - 1; j++ {
+	for j := 0; j < len(arr)-1; j++ {
 		max := arr[j]
 		maxIndex := j
 		// 2.遍历后面 1----[len(arr) - 1] 比较
@@ -18,12 +18,13 @@ func SelectSort(arr *[6]int) {
 			}
 		}
 		// 交换
+		// 如果最大值的下标不等于j
 		if maxIndex != j {
 			arr[j], arr[maxIndex] = arr[maxIndex], arr[j]
 		}
 		fmt.Println(arr)
 	}
-	}
+}
 
 func main() {
 	// 定义一个数组
