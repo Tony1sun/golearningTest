ff68c36c6183b5409efcb977c5bf874724509167
Tony1sun <jacktony822@gmail.com>
2021-10-20 15:28:09 +0800

    add

--- day3/selectsort/main.go
+++ day3/selectsort/main.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // 选择排序
 // 编写函数selectSort 完成排序
 // 注释
-func SelectSort(arr *[6]int) {
+func SelectSort(arr *[800000]int) {
 	// 1.假设 arr[0] 是最大值
-	for j := 0; j < len(arr) - 1; j++ {
+	for j := 0; j < len(arr)-1; j++ {
 		max := arr[j]
 		maxIndex := j
 		// 2.遍历后面 1----[len(arr) - 1] 比较
@@ -21,15 +25,21 @@ func SelectSort(arr *[6]int) {
 		if maxIndex != j {
 			arr[j], arr[maxIndex] = arr[maxIndex], arr[j]
 		}
-		fmt.Println(arr)
+		// fmt.Println(arr)
 	}
-	}
+}
 
 func main() {
 	// 定义一个数组
-	arr := [6]int{10, 34, 19, 100, 80, 789}
-	fmt.Println(arr)
+	// arr := [6]int{10, 34, 19, 100, 80, 789}
+	var arr [800000]int
+	for i := 0; i < 800000; i++ {
+		arr[i] = rand.Intn(900000)
+	}
+	start := time.Now().Unix()
 	SelectSort(&arr)
-	fmt.Println("main函数")
-	fmt.Println(arr)
+	end := time.Now().Unix()
+	fmt.Printf("选择排序耗时=%d秒", end-start)
+	// fmt.Println("main函数")
+	// fmt.Println(arr)
 }
